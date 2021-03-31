2185720deabdd896435c8b1a8b6a06bcdb8213e8
Dor Genosar <dor.genosar@outlook.com>
2021-03-31 11:57:04 +0300

    UI Progress 31-Mar-21 12pm

--- ArDOS/ArdosUI/MainForm.Designer.cs
+++ ArDOS/ArdosUI/MainForm.Designer.cs
@@ -29,23 +29,112 @@ namespace ArDOS.UI
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemRefresh = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItemQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemVersion = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileSystemWatcher = new System.IO.FileSystemWatcher();
+            this.contextMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).BeginInit();
             this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = global::ArDOS.UI.Properties.Resources.ArdosIcon;
+            this.notifyIcon.Text = "ArDOS";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemRefresh,
+            this.toolStripSeparator1,
+            this.toolStripMenuItemQuit,
+            this.toolStripMenuItemVersion});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.contextMenuStrip.Size = new System.Drawing.Size(221, 82);
+            // 
+            // toolStripMenuItemRefresh
+            // 
+            this.toolStripMenuItemRefresh.Name = "toolStripMenuItemRefresh";
+            this.toolStripMenuItemRefresh.Size = new System.Drawing.Size(220, 24);
+            this.toolStripMenuItemRefresh.Text = "Refresh all extensions";
+            this.toolStripMenuItemRefresh.Click += new System.EventHandler(this.toolStripMenuItemRefresh_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(217, 6);
+            // 
+            // toolStripMenuItemQuit
+            // 
+            this.toolStripMenuItemQuit.Name = "toolStripMenuItemQuit";
+            this.toolStripMenuItemQuit.Size = new System.Drawing.Size(220, 24);
+            this.toolStripMenuItemQuit.Text = "Quit ArDOS";
+            this.toolStripMenuItemQuit.Click += new System.EventHandler(this.toolStripMenuItemQuit_Click);
+            // 
+            // toolStripMenuItemVersion
+            // 
+            this.toolStripMenuItemVersion.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripMenuItemVersion.Enabled = false;
+            this.toolStripMenuItemVersion.Font = new System.Drawing.Font("Segoe UI", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.toolStripMenuItemVersion.Name = "toolStripMenuItemVersion";
+            this.toolStripMenuItemVersion.Size = new System.Drawing.Size(220, 24);
+            this.toolStripMenuItemVersion.Text = "ArDOS v1.0.0";
+            this.toolStripMenuItemVersion.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            // 
+            // fileSystemWatcher
+            // 
+            this.fileSystemWatcher.EnableRaisingEvents = true;
+            this.fileSystemWatcher.NotifyFilter = ((System.IO.NotifyFilters)((System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.LastWrite)));
+            this.fileSystemWatcher.SynchronizingObject = this;
+            this.fileSystemWatcher.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher_Changed);
+            this.fileSystemWatcher.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher_Changed);
+            this.fileSystemWatcher.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher_Changed);
+            this.fileSystemWatcher.Error += new System.IO.ErrorEventHandler(this.fileSystemWatcher_Error);
+            this.fileSystemWatcher.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher_Renamed);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(582, 553);
+            this.ClientSize = new System.Drawing.Size(182, 153);
             this.Font = new System.Drawing.Font("Segoe UI Semilight", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.Color.Black;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(200, 200);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(200, 200);
             this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.Text = "ArDOS";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.contextMenuStrip.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.IO.FileSystemWatcher fileSystemWatcher;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemRefresh;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemQuit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemVersion;
     }
 }
 
--- ArDOS/ArdosUI/MainForm.cs
+++ ArDOS/ArdosUI/MainForm.cs
@@ -7,15 +7,138 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ArDOS.UI
 {
     public partial class MainForm : Form
     {
+        public Dictionary<string, NotifyIcon> Plugins { get; protected set; }
+
         public MainForm()
         {
             InitializeComponent();
+            this.Plugins = new Dictionary<string, NotifyIcon>();
+            this.fileSystemWatcher.Path = Properties.Settings.Default.ExtensionsDirectoryPath;
+            this.StartScheduler();
         }
 
+        protected void SchedulePluginsToRun()
+        {
+            var info = new DirectoryInfo(Properties.Settings.Default.ExtensionsDirectoryPath);
+            var files = info.GetFiles();
+            foreach (var fileInfo in files)
+            {
+                var parts = fileInfo.Name.Split('.', 4);
+                string intervalString;
+                if (parts.Length == 4) intervalString = parts[2];
+                else if (parts.Length == 3) intervalString = parts[1];
+                else continue;
+
+                if (intervalString.Equals(""))
+                {
+                    // Scheduler.Run(fileInfo.FullName);
+                }
+                else
+                {
+                    bool runOnOpen;
+                    if (runOnOpen = intervalString[^1] == '+')
+                        intervalString = intervalString[0..^1];
+                    if (!int.TryParse(intervalString[0..^1], out int intervalInt)) continue;
+                    TimeSpan interval;
+                    switch (intervalString[^1])
+                    {
+                        case 's':
+                            interval = TimeSpan.FromSeconds(intervalInt);
+                            break;
+                        case 'm':
+                            interval = TimeSpan.FromMinutes(intervalInt);
+                            break;
+                        case 'h':
+                            interval = TimeSpan.FromHours(intervalInt);
+                            break;
+                        case 'd':
+                            interval = TimeSpan.FromDays(intervalInt);
+                            break;
+                    }
+
+                    // Scheduler.Schedule(fileInfo.FullName, interval);
+                }
+            }
+        }
+
+        protected void StartScheduler()
+        {
+
+            SchedulePluginsToRun();
+        }
+
+        protected void StopScheduler()
+        {
+            // Close all notify icons
+            foreach (var pair in this.Plugins) pair.Value.Dispose();
+            this.Plugins.Clear();
+
+
+        }
+
+        protected void RestartScheduler()
+        {
+            this.StopScheduler();
+            this.StartScheduler();
+        }
+
+        protected void Runner_OnOutputReady(object sender, RunnerOutputEventArgs e)
+        {
+            if (!this.Plugins.TryGetValue(e.Path, out NotifyIcon plugin))
+                plugin = new NotifyIcon();
+            var contextMenuStrip = new ContextMenuStrip();
+            // var menu = Parser.ParsePlugin(e.Output);
+            // Create a new context menu strip
+            plugin.ContextMenuStrip = contextMenuStrip;
+            this.Plugins[e.Path] = plugin;
+        }
+
+        private void toolStripMenuItemRefresh_Click(object sender, EventArgs e)
+        {
+            var info = new DirectoryInfo(Properties.Settings.Default.ExtensionsDirectoryPath);
+            var files = info.GetFiles();
+            foreach (var fileInfo in files) ;
+            // Scheduler.Run(fileInfo.FullName);
+        }
+
+        #region Winforms event subcribers
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            new ConfigForm().ShowDialog();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void toolStripMenuItemQuit_Click(object sender, EventArgs e)
+        {
+            this.StopScheduler();
+            this.Close();
+        }
+
+        private void fileSystemWatcher_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            this.RestartScheduler();
+        }
+
+        private void fileSystemWatcher_Error(object sender, System.IO.ErrorEventArgs e)
+        {
+            // Some error code
+        }
+
+        private void fileSystemWatcher_Renamed(object sender, System.IO.RenamedEventArgs e)
+        {
+            this.RestartScheduler();
+        }
+
+        #endregion
     }
 }
